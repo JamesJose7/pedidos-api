d23d56b6cf08618c11f577924c7d6d808e89dd45
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2018-11-18 19:50:41 -0500

    API Configuration

---
+++ src/main/java/com/arqapps/pedidosapi/ws/Configuration.java
@@ -0,0 +1,8 @@
+package com.arqapps.pedidosapi.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class Configuration extends Application {
+}
