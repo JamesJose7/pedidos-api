5993f5a3258158d2eeb9e2bd740f9db39336e4cf
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2018-11-18 19:48:44 -0500

    added dao and in-memory database

--- src/main/java/com/arqapps/pedidosapi/dao/PedidoDao.java
+++ src/main/java/com/arqapps/pedidosapi/dao/PedidoDao.java
@@ -1,4 +1,17 @@
 package com.arqapps.pedidosapi.dao;
 
+import com.arqapps.pedidosapi.database.Database;
+import com.arqapps.pedidosapi.model.Pedido;
+
+import java.util.List;
+
 public class PedidoDao {
+    private static List<Pedido> pedidos = Database.getPedidos();
+
+    public PedidoDao() {
+        pedidos.add(new Pedido(1, "pedido", 240L, false));
+        pedidos.add(new Pedido(2, "pedido", 240L, false));
+        pedidos.add(new Pedido(3, "pedido", 240L, false));
+        pedidos.add(new Pedido(4, "pedido", 240L, false));
+    }
 }
--- src/main/java/com/arqapps/pedidosapi/database/Database.java
+++ src/main/java/com/arqapps/pedidosapi/database/Database.java
@@ -1,4 +1,14 @@
 package com.arqapps.pedidosapi.database;
 
+import com.arqapps.pedidosapi.model.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+    private static List<Pedido> pedidos = new ArrayList<>();
+
+    public static List<Pedido> getPedidos() {
+        return pedidos;
+    }
 }
